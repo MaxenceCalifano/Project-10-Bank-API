ab0f245b54ff9f039e9f28f5f774592a1f9f19f1
Maxence <maxence.califano@gmail.com>
2023-02-23 10:13:32 +0200

    create default rooting

--- front/src/App.js
+++ front/src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
+import Root from './Pages/Root';
+import Home from './Pages/Home';
+import ErrorPage from './Pages/ErrorPage';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path:"/",
+        element:<Home />
+      },
+     /*  {
+        path:"/about",
+        element: <About />
+      },
+      {
+        path:"/accommodation/:id",
+        element: <Accommodation />
+      } */
+    ]
+  }
+])
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-    </div>
+    <RouterProvider router={router} />
   );
 }
 
