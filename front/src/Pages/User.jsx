49452afa8b4667dd3e485a33e182d6be7b9e515d
Maxence <maxence.califano@gmail.com>
2023-02-24 09:56:36 +0200

    toggle edit name

--- front/src/Pages/User.jsx
+++ front/src/Pages/User.jsx
@@ -1,11 +1,24 @@
 import Account from '../components/Account';
 import styles from '../css/user.module.css'
+import { useState } from 'react';
 
 function User() {
+    const [editName, toggleEdit] = useState(false);
+
     return (
         <main className={styles.main}>
             <h1>Welcome back {/*user */}!</h1>
-            <button>Edit Name</button>
+            {
+                !editName ?
+                    <button onClick={() => toggleEdit(true)}>Edit Name</button>
+                    :
+                    <>
+                        <input type="text" />
+                        <input type="text" />
+                        <button onClick={() => toggleEdit(false)}>save</button>
+                        <button onClick={() => toggleEdit(false)}>cancel</button>
+                    </>
+            }
             <Account />
         </main>
     );
