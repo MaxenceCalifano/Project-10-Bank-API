577596b376b672202ec06dbd2a1198874a6b5786
Maxence <maxence.califano@gmail.com>
2023-02-28 10:01:24 +0200

    sign-in

--- front/src/Pages/Sign-in.jsx
+++ front/src/Pages/Sign-in.jsx
@@ -1,7 +1,25 @@
 import styles from '../css/signIn.module.css'
 import userCircle from '../assets/account_circle.svg'
 
+import { useState } from 'react'
+
 function SignIn() {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const login = (e) => {
+        e.preventDefault()
+        fetch("http://localhost:3001/api/v1/user/login", {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                "email": email,
+                "password": password
+            })
+        })
+    }
+
     return (
         <main className={styles.bgBlack}>
             <section className={styles.signInContent}>
@@ -9,15 +27,15 @@ function SignIn() {
                 <h1>Sign In</h1>
                 <form>
                     <div className={styles.inputWrapper}>
-                        <label htmlFor="username">Username</label><input type="text" id="username" />
+                        <label htmlFor="username">Username</label><input type="text" id="username" onChange={(e) => setEmail(e.target.value)} />
                     </div>
                     <div className={styles.inputWrapper}>
-                        <label htmlFor="password">Password</label><input type="password" id="password" />
+                        <label htmlFor="password">Password</label><input type="password" id="password" onChange={(e) => setPassword(e.target.value)} />
                     </div>
                     <div className={styles.inputRemember}>
                         <input type="checkbox" id="remember-me" /><label htmlFor="remember-me">Remember me</label>
                     </div>
-                    <button className={styles.signInButton}>Sign In</button>
+                    <button formNoValidate onClick={login} className={styles.signInButton}>Sign In</button>
                 </form>
             </section>
         </main>
