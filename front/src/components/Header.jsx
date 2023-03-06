0d15b91ab454ffe80f1240a2fe638bd1a774ddb0
Maxence <maxence.califano@gmail.com>
2023-03-06 12:25:35 +0200

    header sign in

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,17 +1,31 @@
 import styles from '../css/header.module.css'
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import userCircle from '../assets/userCircle.png';
 
 function Header() {
+    const userStatus = useSelector(state => state.user.status)
+    console.log(userStatus)
 
     return (
         <header>
             <h1><Link to={'/'}><span className={styles.argent}>ARGENT</span><span className={styles.bank}>BANK</span></Link></h1>
-            {
-                /**
-                 * If is signed in show signout button and profile
-                 * else show sign in
-                 */
-            }
+            <div className={styles.user}>
+                {
+                    userStatus === 'offline' ?
+                        <Link to="sign-in">
+                            <img src={userCircle} alt="" />
+                            <p>Sign In</p>
+                        </Link>
+                        :
+                        <p>En ligne</p>
+                    /**
+                     * If is signed in show signout button and profile
+                     * else show sign in
+                     */
+                }
+            </div>
+
         </header>
     );
 }
