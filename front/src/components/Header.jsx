c9544cbca6f566839f30395fd7b4aca52f6b5d8f
Maxence <maxence.califano@gmail.com>
2023-03-11 22:19:24 +0200

    build: header usernav

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -2,23 +2,35 @@ import styles from '../css/header.module.css'
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import userCircle from '../assets/userCircle.png';
+import signOut from '../assets/signOut.png';
 
 function Header() {
-    const userStatus = useSelector(state => state.user.userStatus)
-    console.log(userStatus)
+    const user = useSelector(state => state.user)
+    console.log(user)
 
     return (
         <header>
             <h1><Link to={'/'}><span className={styles.argent}>ARGENT</span><span className={styles.bank}>BANK</span></Link></h1>
             <div className={styles.user}>
                 {
-                    userStatus === 'offline' ?
+                    user.userStatus === 'offline' ?
                         <Link to="sign-in">
                             <img src={userCircle} alt="" />
                             <p>Sign In</p>
                         </Link>
                         :
-                        <p>En ligne</p>
+                        <nav className={styles.userNav}>
+                            <Link to={"/user"}>
+                                <img src={userCircle} alt="" />
+                                {user.userData.firstName}
+                            </Link>
+
+                            <Link to={"/"}>
+                                <img src={signOut} alt="" />
+                                <p>Sign Out</p>
+                            </Link>
+
+                        </nav>
                     /**
                      * If is signed in show signout button and profile
                      * else show sign in
